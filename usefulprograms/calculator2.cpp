644adccf3bb9fbff13f807a733472682efd8b241
Methsaan Wick <methsaan@gmail.com>
2018-05-17 08:00:21 -0400

    update

--- usefulprograms/calculator2.cpp
+++ usefulprograms/calculator2.cpp
@@ -1,49 +1,69 @@
 #include<iostream>
 #include<cmath>
 #include<string>
-#include<fstream>
 
 using namespace std;
-void logic() {
+bool logic() {
+	bool running = true;
 	if (cin.fail()){
-               cin.clear();
-               cout << "Invalid number.\a\n";
-	       throw exception();
+                cin.clear();
+        	cout << "Invalid number.\a\n";
+		bool running = false;
 	}
+	return running;
 }
-void operror(string x) {
-	if (x != "hypotenuse") && (x != "square_root") && (x != "cube_root") {
+bool operror(string x) {
+	bool running = true;
+	if ((x != "hypotenuse") && (x != "square_root") && (x != "cube_root")) {
 		cout << "Invalid operator.\n";
+		bool running = false;
 	}
+	return running;
 }
 int main()
 {
-	double f;
-	double s;
-	double squart;
-	cout << "################################################\n";
-	cout << "########### Welcome to calculator 2 ############\n";
-	cout << "################################################\n";
-	cout << "Options: hypotenuse, square_root, cube_root\n";
-	cout << "Enter operator: ";
-	string oper;
-	cin >> oper;
-	operror(oper);
-	cout << "Enter first number: ";
-	cin >> f;
-	logic();
-	cout << "Enter second number: ";
-	cin >> s;
-	logic();
-	if (oper == "Hypotenuse") {
-		squart = pow((f * f) + (s * s), 0.5);
-		cout << "Hypo" << endl;
-	}else if (oper == "square_root") {
-		squart = pow(f, 0.5);
-		cout << squart << endl;
-	}else if (oper == "cube_root") {
-		double third = 1 / 3;
-		squart = pow(f, third);
-		cout << squart << endl;
+	bool running = true;
+	while (running == true){
+		double f;
+		double s;
+		double squart;
+		cout << "################################################\n";
+		cout << "########### Welcome to calculator 2 ############\n";
+		cout << "################################################\n";
+		cout << "Options: hypotenuse, square_root, cube_root\n";
+		cout << "Enter operator: ";
+		string oper;
+		cin >> oper;
+		bool opbreak = operror(oper);
+		if (opbreak == false){
+			break;
+		}
+		cout << "Enter first number: ";
+		cin >> f;
+		bool numfalse1 = logic();
+		if (numfalse1 == false){
+			break;
+		}
+		cout << "Enter second number: ";
+		cin >> s;
+		bool numfalse2 = logic();
+		if (numfalse2 = false) {
+			break;
+		}
+		if (oper == "Hypotenuse") {
+			double fsqr = f * f;
+			double ssqr = s * s;
+			double sqrsum = fsqr + fsqr;
+			squart = pow(sqrsum, 0.5);
+			cout << squart << endl;
+		}else if (oper == "square_root") {
+			squart = pow(f, 0.5);
+			cout << squart << endl;
+		}else if (oper == "cube_root") {
+			double third = 1 / 3;
+			squart = pow(f, third);
+			cout << squart << endl;
+		}
 	}
+	return 0;
 }
