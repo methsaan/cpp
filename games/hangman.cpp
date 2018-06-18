33981194eba90e39f9acc3ed0dd819ceb96515c3
Methsaan <methsaan@gmail.com>
2018-06-17 21:33:30 -0400

    update

--- games/hangman.cpp
+++ games/hangman.cpp
@@ -3,9 +3,14 @@
 #include<cmath>
 #include<ctime>
 #include<cstdlib>
-#include<cstring>
 
 using namespace std;
+
+void printarray(string arrayf[], string join){
+	for (int x = 0; x < sizeof(arrayf); x++){
+		cout << arrayf[x] << join;
+	}
+}
 
 int main(){
 	string word;
@@ -13,13 +18,36 @@ int main(){
 	cin >> word;
 	system("clear");
 	string letter = "";
+	string progress[word.length()];
+	for (int x = 0; x < word.length(); x++){
+		progress[x] = "_";
+	}
+	string strikes[word.length()+2];
+	strikes[0] = "{";
+	for (int x = 1; x < word.length()+1; x++){
+		strikes[x] = ".";
+	}
+	strikes[word.length()+2] = "}";
+	int strike = 0;
 	while (true){
 		cout << "Enter a character: ";
-		if (word.find(letter)){
+		cin >> letter;
+		if (!word.find(letter)){
 			cout << "Yes\n";
+			progress[3] = "C";
+			progress[4] = "+";
+			progress[5] = "+";
 		}else{
 			cout << "no\n";
+			printarray(strikes, "");
+			strike++;
+			strikes[strike] = "#";
+			if (strike == 10){
+				cout << "GAME OVER";
+				break;
+			}
 		}
+		printarray(progress, " ");
 	}
 	return 0;
 }
