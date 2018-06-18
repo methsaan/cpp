047e6bf6d3a3ab066f619e35b415ea68524b445c
Methsaan <methsaan@gmail.com>
2018-06-17 20:23:09 -0400

    update

--- games/hangman.cpp
+++ games/hangman.cpp
@@ -3,19 +3,25 @@
 #include<cmath>
 #include<ctime>
 #include<cstdlib>
+#include<cstring>
+#include "boost/algorithm/string.hpp"
+
+using namespace std;
+using namespace boost;
 
 int main(){
 	string word;
-	cout << "Enter a word";
+	cout << "Enter a word: ";
 	cin >> word;
 	system("clear");
 	string letter = "";
 	while (true){
 		cout << "Enter a character: ";
-		if (word.find(letter)){
+		if (contains(word, letter) == true){
 			cout << "Yes";
 		}else{
 			cout << "no";
 		}
 	}
+	return 0;
 }
