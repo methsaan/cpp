41f699de4c1becaa2d5b449d8b61dea0bd01f516
Methsaan <methsaan@gmail.com>
2018-06-17 20:12:56 -0400

    update

--- games/hangman.cpp
+++ games/hangman.cpp
@@ -2,11 +2,20 @@
 #include<cstring>
 #include<cmath>
 #include<ctime>
+#include<cstdlib>
 
 int main(){
 	string word;
 	cout << "Enter a word";
 	cin >> word;
-	while (){
+	system("clear");
+	string letter = "";
+	while (true){
+		cout << "Enter a character: ";
+		if (word.find(letter)){
+			cout << "Yes";
+		}else{
+			cout << "no";
+		}
 	}
 }
