b9382eaa888ee46e38c2f7a75d61d404aff47280
Methsaan <methsaan@gmail.com>
2018-06-23 20:52:54 -0400

    update

--- games/hangman.cpp
+++ games/hangman.cpp
@@ -19,17 +19,16 @@ int main(){
 	cin >> word;
 	system("clear");
 	string letter = "";
-	cout << word.length;
-	/*string progress[word.length];
-	for (int x = 0; x < word.length; x++){
+	string progress[word.length()];
+	for (int x = 0; x < word.length(); x++){
 		progress[x] = "_";
 	}
-	string strikes[word.length+2];
+	string strikes[word.length()+2];
 	strikes[0] = "{";
-	for (int x = 1; x < floor(((word.length/5)*3) + 0.5); x++){
+	for (int x = 1; x < floor(((word.length()/5)*3) + 0.5); x++){
 		strikes[x] = ".";
 	}
-	strikes[word.length+2] = "}";
+	strikes[word.length()+2] = "}";
 	int strike = 0;
 	while (true){
 		cout << "Enter a character: ";
@@ -50,6 +49,6 @@ int main(){
 			}
 		}
 		printarray(progress, " ");
-	}*/
+	}
 	return 0;
 }
