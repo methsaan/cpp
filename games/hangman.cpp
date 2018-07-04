d3a9aa05be371e25c143f21b03f070baf921cf43
Methsaan <methsaan@gmail.com>
2018-07-04 08:03:57 -0400

    update

--- games/hangman.cpp
+++ games/hangman.cpp
@@ -8,47 +8,13 @@
 using namespace std;
 
 void printarray(string arrayf[], string join){
-	for (int x = 0; x < arrayf->size(); x++){
-		cout << arrayf[x] << join;
+	for (int x = 0; x < sizeof(arrayf)/sizeof(arrayf[0]); x++){
+		cout << arrayf[x] << join << endl;
 	}
 }
 
 int main(){
-	string word;
-	cout << "Enter a word: ";
-	cin >> word;
-	system("clear");
-	string letter = "eq";
-	string progress[word.length()];
-	for (int x = 0; x < word.length(); x++){
-		progress[x] = "_";
-	}
-	string strikes[word.length()+2];
-	strikes[0] = "{";
-	for (int x = 1; x < floor(((word.length()/5)*3) + 0.5); x++){
-		strikes[x] = ".";
-	}
-	strikes[word.length()+2] = "}";
-	int strike = 0;
-	while (true){
-		cout << "Enter a character: ";
-		cin >> letter;
-		if (!word.find(letter)){
-			cout << "Yes\n";
-			progress[3] = "C";
-			progress[4] = "+";
-			progress[5] = "+";
-		}else{
-			cout << "no\n";
-			printarray(strikes, "");
-			strike++;
-			strikes[strike] = "#";
-			if (strike == 10){
-				cout << "GAME OVER";
-				break;
-			}
-		}
-		printarray(progress, " ");
-	}
+	string x[] = {"12", "5123", "46", "124", "362", "352", "6243"};
+	printarray(x, ",-");
 	return 0;
 }
