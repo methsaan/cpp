6b230ef2ea5bbf843826b204c2687da4aaf2d8ab
Methsaan <methsaan@gmail.com>
2018-07-12 10:18:34 -0400

    update

--- games/hangman.cpp
+++ games/hangman.cpp
@@ -5,11 +5,12 @@
 #include<cstdlib>
 #include<array>
 #include<cstdbool>
+#include<string>
 
 using namespace std;
 
 template<size_t SIZE>
-void printstringarray(string start, array<int, SIZE> arrayf, string join, string end){
+void printStringArray(string start, array<int, SIZE> arrayf, string join, string end){
 	cout << start;
 	for (int x = 0; x < arrayf.size(); x++){
 		if (x < arrayf.size()-1){
@@ -20,15 +21,27 @@ void printstringarray(string start, array<int, SIZE> arrayf, string join, string
 	}
 	cout << end;
 }
-
 int main(){
 	string word;
 	while (true){
-		array<string, 8> userprogress;
-		array<string, 8> strikepict;
 		cout << "Enter a word: ";
-		getline(cin, word);
-		cout << word << endl;
+		cin >> word;
+		unsigned int len1 = word.length();
+		int len2 = len1;
+		array<string, len2> userprogress = {"_", "_", "_", "_", "_"};
+		array<string, len2+2> strikepict = {"{", ".", ".", ".", ".", ".", "}"};
+		//system("clear");
+		cout << word.length() << " " << userprogress[1] << " " << strikepict[0] endl;
+		/*get<0>(userprogress) = "{";
+		for (int x = 1; x < word.length();x++){
+			get<x>(strikepict) = ".";
+		}
+		get<word.length()()+1>(strikepict) = "}";
+		for (int x = 0;x < word.length();x++){
+			get<x>(userprogress) = "_";
+		}*/
+		//printStringArray("", strikepict, " ", "");
+		//printStringArray("", userprogress, " ", "");
 	}
 	return 0;
 }
