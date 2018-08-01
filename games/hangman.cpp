227f106577ef72a9b07987d690976abe5406d398
Methsaan <methsaan@gmail.com>
2018-07-31 21:06:05 -0400

    update

--- games/hangman.cpp
+++ games/hangman.cpp
@@ -6,22 +6,40 @@
 #include<array>
 #include<cstdbool>
 #include<string>
+#include<cstdio>
 
 using namespace std;
 
 int main(){
 	string word;
 	while (true){
-		array<string, 8> userprogress;
-		array<string, 8> strikepict;
+		//initialization of word, userprogress, and strikepict
 		cout << "Enter a word: ";
 		cin >> word;
 		system("clear");
 		cout << word << endl;
 		int wordlen = word.size();
+		array<string, wordlen> userprogress;
+		array<string, wordlen+2> strikepict;
 		char wordarr[wordlen];
 		for (int x = 0;x < wordlen;x++){
 			wordarr[x] = word.at(x);
+		}
+		for (int x = 0;x < wordlen;x++){
+			userprogress[x] = "_";
+		}
+		int a;
+		strikepict[0] = "{";
+		for (a = 1; a < wordlen-1; a++){
+			strikepict[a] = "#";
+		}
+		strikepict[wordlen] = "}";
+		//loop
+		for (int x = 0; x < strikepict.size(); x++){
+			cout << strikepict[x] << endl;
+		}
+		for (int x = 0; x < userprogress.size(); x++){
+			cout << userprogress[x] << endl;
 		}
 	}
 	return 0;
