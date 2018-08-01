dcd0eef21ba47b3fc008bfcdc69aa36b038cc25c
Methsaan <methsaan@gmail.com>
2018-08-01 09:58:09 -0400

    update

--- games/hangman.cpp
+++ games/hangman.cpp
@@ -10,37 +10,42 @@
 
 using namespace std;
 
+void printarr(int len, string start, string arr[], string end, string join){
+	cout << start;
+	for (int x = 0; x < len; x++){
+		if (x < len){
+			cout << arr[x] << join;
+		}else{
+			cout << arr[x];
+		}
+	}
+	cout << end;
+}
 int main(){
 	string word;
 	while (true){
-		//initialization of word, userprogress, and strikepict
+		//initialization of wordarr, wordlen, word, userprogress, and strikepict
 		cout << "Enter a word: ";
 		cin >> word;
 		system("clear");
 		cout << word << endl;
 		int wordlen = word.size();
-		array<string, wordlen> userprogress;
-		array<string, wordlen+2> strikepict;
 		char wordarr[wordlen];
 		for (int x = 0;x < wordlen;x++){
 			wordarr[x] = word.at(x);
 		}
-		for (int x = 0;x < wordlen;x++){
+		string userprogress[wordlen+1];
+		string strikepict[wordlen+3];
+		strikepict[0] = "{";
+		for (int x = 1; x < wordlen+1; x++) {
+			strikepict[x] = ".";
+		}
+		strikepict[wordlen+1] = "}";
+		for (int x = 0; x < wordlen+1; x++){
 			userprogress[x] = "_";
 		}
-		int a;
-		strikepict[0] = "{";
-		for (a = 1; a < wordlen-1; a++){
-			strikepict[a] = "#";
-		}
-		strikepict[wordlen] = "}";
-		//loop
-		for (int x = 0; x < strikepict.size(); x++){
-			cout << strikepict[x] << endl;
-		}
-		for (int x = 0; x < userprogress.size(); x++){
-			cout << userprogress[x] << endl;
-		}
+		printarr(wordlen, "", userprogress, "\n", " ");
+		printarr(wordlen+2, "", strikepict, "\n", "");
 	}
 	return 0;
 }
