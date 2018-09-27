c2a851cdb18937d29085901c1a875f05f04a48a3
Methsaan <methsaan@gmail.com>
2018-09-26 20:55:24 -0400

    update

--- games/hangman.cpp
+++ games/hangman.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<stdio.h>
 #include<ctype.h>
+#include<stdlib.h>
 using namespace std;
 
 void printarr(int len, string start, char arr[], string end, string join){
@@ -28,7 +29,32 @@ int main(){
 			break;
 		}
 	}
-	printarr(wordlen, "word: {", word, "}\n", ", ");
-
+	char progress[sizeof(word)/sizeof(*word)];
+	for (int x = 0; x < sizeof(word)/sizeof(*word)];x++){
+		progress[x] = '_';
+	}
+	char strikes[sizeof(word/sizeof(*word)+2)];
+	strikes[0] = '{';
+	for (int x = 1; x < sizeof(word)/sizeof(*word); x++){
+		strikes[x] = '.';
+	}
+	strike[sizeof(word)/sizeof(*word)+1] = '}';
+	int strike = 0;
+	while (1){
+		char letter;
+		cout << "Enter letter: ";
+		cin >> letter;
+		int cnt;
+		for (int x = 0; x < sizeof(word)/sizeof(*word); x++){
+			if (letter == word[x]){
+				cnt++;
+			}
+		}
+		if (cnt > 0){
+			cout << "found" << endl;
+		}else {
+			cout << "not found" << endl;
+		}
+	}
 	return 0;
 }
