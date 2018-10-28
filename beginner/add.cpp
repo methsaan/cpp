57aa1437daba77613fb31329bb829459fe0accb7
Mohan Wickramasinghe <mwickramasinghe@nbsps.com>
2018-10-28 14:20:52 -0400

    beginner code samples

---
+++ beginner/add.cpp
@@ -0,0 +1,13 @@
+// Adding 2 integers and printing //
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    int x=30;
+    int y=22;
+    cout << "Adding " << x << " and " << y << " gives " << x+y << endl;
+}
---
+++ beginner/math1.cpp
@@ -0,0 +1,18 @@
+// math operations //
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    int a=4;
+    int b=9;
+    int c=a*b;
+    int d=a-b;
+    int e=a+b;
+    cout << "numbers: a = " << a << " and b = " << b << endl;
+    cout << "multiple a*b = " << c << endl;
+    cout << "substract a-b = " << d << endl;
+    cout << "add a+b = " << e << endl;
+}
