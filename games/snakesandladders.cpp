576d3e44d5ba60a1a4824c01e9b86c4275d9b0f5
Methsaan <methsaan@gmail.com>
2018-11-07 19:57:46 -0500

    finishing

--- games/snakesandladders.cpp
+++ games/snakesandladders.cpp
@@ -25,6 +25,7 @@ int index(int e, int arr[], int len){
 	return index;
 }
 int main(int argc, char **argv){
+	srand(time(0));
 	int p1square = 0;
 	int p2square = 0;
 	int temp;
@@ -32,47 +33,72 @@ int main(int argc, char **argv){
 	int ladderspots[] = {7, 14, 28, 41, 67, 80};
 	int snakeland[] = {4, 11, 23, 45, 24, 88};
 	int ladderland[] = {13, 37, 84, 58, 74, 100};
-	int max = 100;
+	const int max = 100;
+	int access;
 	while (1) {
 		temp = (rand()%6)+1;
 		cout << "Player 1: Press enter to roll\n";
 		getchar();
 		cout << "Rolling ... " << temp << endl;
 		p1square += temp;
-		cout << "Player 1 is on square number: " << p1square << endl;
 		getchar();
 		if (isinint(p1square, snakespots, 6)){
 			p1square = snakeland[index(p1square, snakespots, 6)];
 			cout << "Player 1 landed on a snake" << endl;
-			getchar();
 			cout << "Player 1 is on square number " << p1square << endl;
 			getchar();
-		}
-		if (isinint(p1square, ladderspots, 6)){
+		}else if (isinint(p1square, ladderspots, 6)){
 			p1square = ladderland[index(p1square, ladderspots, 6)];
 			cout << "Player 1 landed on a ladder" << endl;
+			cout << "Player 1 is on square number " << p1square << endl;
 			getchar();
+		}else if (p1square > max){
+			access = p1square-max;
+			p1square = max-access;
+			cout << "Player 1 has no room to land ...\n";
+			cout << "Player 1 is now on " << p1square << endl;
+			getchar();
+		}else {
 			cout << "Player 1 is on square number " << p1square << endl;
 			getchar();
 		}
-		if (p1square >= max){
-			cout << "The winner is player 1" << endl;
+		if (p1square == max){
+			cout << "The winner is Player 1\n";
+			getchar();
 			break;
 		}
 		temp = (rand()%6)+1;
 		cout << "Player 2: Press enter to roll\n";
 		getchar();
 		cout << "Rolling ... " << temp << endl;
+		getchar();
 		p2square += temp;
 		cout << "Player 2 is on square number: " << p2square << endl;
 		getchar();
 		if (isinint(p2square, snakespots, 6)) {
 			p2square = snakeland[index(p2square, snakespots, 6)];
 			cout << "Player 2 landed on a snake" << endl;
-			getchar();
 			cout << "Player 2 is on square number " << p2square << endl;
 			getchar();
+		}else if (isinint(p2square, ladderspots, 6)){
+			p2square = ladderland[index(p2square, ladderspots, 6)];
+			cout << "Player 2 landed on a ladder" << endl;
+			cout << "Player 2 is on square number " << p2square << endl;
+			getchar();
+		}else if (p2square > max){
+			access = p2square-max;
+			p2square = max-access;
+			cout << "Player 2 has no room to land ...\n";
+			cout << "Player 2 is now on " << p2square << endl;
+			getchar();
+		}else {
+			cout << "Player 2 is on square number " << p1square << endl;
+			getchar();
 		}
-		if (isinint(p2square, ladderspots
+		if (p1square == max){
+			cout << "The winner is Player 2\n";
+			getchar();
+			break;
+		}
 	}
 }
