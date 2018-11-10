1f8efca6df70a0a2229cb853f7170e9d91670cb5
Methsaan Wick@kali <methsaan@gmail.com>
2018-11-10 15:57:57 -0500

    moving tax.cpp and updating class-3.cpp

---
+++ usefulprograms/tax.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char **argv){
+	int price;
+	cout << "Enter price: ";
+	cin >> price;
+	cout << price << " plus tax: ";
+	printf("%.2f\n", ((double)price/100)*13+price);
+}
