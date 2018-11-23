5fc71fe56fb6f27b534863a7d3ab8c104fc35e6c
Methsaan <methsaan@gmail.com>
2018-11-23 18:53:38 -0500

    fixing errors

--- usefulprograms/piglatin.cpp
+++ usefulprograms/piglatin.cpp
@@ -7,6 +7,6 @@ int main(int argc, char *argv[]){
 	string piglatin;
 	cout << "Enter a string: ";
 	cin >> english;
-	piglatin = english.substr(1, english.length-1)+english.substr(0, 1)+"ay";
+	piglatin = english.substr(1, english.length()-1)+english.substr(0, 1)+"ay";
 	cout << piglatin << endl;
 }
