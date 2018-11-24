06c17720157a38c7e0ab11d48d7692425d4bc506
Methsaan <methsaan@gmail.com>
2018-11-24 09:58:56 -0500

    accepting spaces

--- usefulprograms/piglatin.cpp
+++ usefulprograms/piglatin.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[]){
 	string english;
 	string piglatin;
 	cout << "Enter a string: ";
-	cin >> english;
+	getline(cin, english);
 	piglatin = english.substr(1, english.length()-1)+english.substr(0, 1)+"ay";
 	cout << piglatin << endl;
 }
