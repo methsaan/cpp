b9b7b332fadea7fda3de72440ed5f497fab2ad80
Methsaan Wick@kali <methsaan@gmail.com>
2019-01-16 06:33:10 -0500

    moving pound symbol

---
+++ usefulprograms/consoleAnimation.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+	int xPos = 0;
+	while (xPos < 30) {
+		system("clear");
+		cout << "################################\n";
+		cout << "##                            ##\n";
+		cout << "##                            ##\n";
+		cout << "##                            ##\n";
+		cout <<	"##                            ##\n";
+		cout << "##                            ##\n";
+		cout << "##                            ##\n";
+		cout << "##                            ##\n";
+		cout << "##                            ##\n";
+		cout << "##                            ##\n";
+		cout << "## ";
+		for (int i = 0; i < xPos; i++) {
+			cout << " ";
+		}
+		cout << "#";
+		for (int i = 0; i < 26-xPos; i++){
+			cout << " ";
+		}
+		cout << "##\n";
+		cout << "##                            ##\n";
+		cout << "##                            ##\n";
+		cout << "##                            ##\n";
+		cout << "##                            ##\n";
+		cout << "##                            ##\n";
+		cout << "##                            ##\n";
+		cout << "##                            ##\n";
+		cout << "##                            ##\n";
+		cout << "##                            ##\n";
+		cout << "##                            ##\n";
+		cout << "################################\n";
+		xPos++;
+		for (unsigned long long int x = 0; x < 10000000; x++) {
+			;
+		}
+	}
+}
