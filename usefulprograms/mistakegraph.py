2eb14b6b708a1f7d4a1773f78f984aba0cca8293
Methsaan <methsaan@gmail.com>
2019-02-10 17:53:42 -0500

    storing mistakes for exercises

--- usefulprograms/mistakegraph.py
+++ usefulprograms/mistakegraph.py
@@ -2,10 +2,17 @@
 
 from tkinter import *
 tk = Tk()
-canvas = Canvas(tk, width=500, height=500, bg="aqua")
-canvas = Canvas(tk, width=1100, height=800, bg="aqua")
-canvas.pack()
 
-canvas.create_line(250, 50, 250, 680, width=5)
-canvas.create_line(250, 680, 1050, 680, width=5)
+fo = open("mistakeTracker", "r")
+
+canvas = Canvas(tk, width=500, height=500, bg="white")
+canvas = Canvas(tk, width=1100, height=800, bg="white")
+canvas.pack()
+canvas.create_line(250, 50, 250, 680, width=10)
+canvas.create_line(250, 680, 1050, 680, width=10)
+mistakes = []
+for x in range(10):
+    mistakes.append(int(fo.readline()[-3:-1]))
+print(mistakes)
+fo.close()
 canvas.mainloop()
