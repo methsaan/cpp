e5919836ccc08331769e0607576d7f3c2ac4a30e
Methsaan <methsaan@gmail.com>
2019-02-13 06:25:45 -0500

    changing border

--- usefulprograms/consoleAnimation.cpp
+++ usefulprograms/consoleAnimation.cpp
@@ -4,42 +4,44 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
-	int xPos = 0;
-	while (xPos < 30) {
-		system("clear");
-		cout << "################################\n";
-		cout << "##                            ##\n";
-		cout << "##                            ##\n";
-		cout << "##                            ##\n";
-		cout <<	"##                            ##\n";
-		cout << "##                            ##\n";
-		cout << "##                            ##\n";
-		cout << "##                            ##\n";
-		cout << "##                            ##\n";
-		cout << "##                            ##\n";
-		cout << "## ";
-		for (int i = 0; i < xPos; i++) {
-			cout << " ";
-		}
-		cout << "#";
-		for (int i = 0; i < 26-xPos; i++){
-			cout << " ";
-		}
-		cout << "##\n";
-		cout << "##                            ##\n";
-		cout << "##                            ##\n";
-		cout << "##                            ##\n";
-		cout << "##                            ##\n";
-		cout << "##                            ##\n";
-		cout << "##                            ##\n";
-		cout << "##                            ##\n";
-		cout << "##                            ##\n";
-		cout << "##                            ##\n";
-		cout << "##                            ##\n";
-		cout << "################################\n";
-		xPos++;
-		for (unsigned long long int x = 0; x < 1000000; x++) {
-			;
+	for (int x = 0; x < 10; x++) {
+		int xPos = 0;
+		while (xPos < 30) {
+			system("clear");
+			cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n";
+			cout << "@@                            @@\n";
+			cout << "@@                            @@\n";
+			cout << "@@                            @@\n";
+			cout <<	"@@                            @@\n";
+			cout << "@@                            @@\n";
+			cout << "@@                            @@\n";
+			cout << "@@                            @@\n";
+			cout << "@@                            @@\n";
+			cout << "@@                            @@\n";
+			cout << "@@ ";
+			for (int i = 0; i < xPos-1; i++) {
+				cout << " ";
+			}
+			cout << "X";
+			for (int i = 0; i < 26-xPos-1; i++){
+				cout << " ";
+			}
+			cout << "@@\n";
+			cout << "@@                            @@\n";
+			cout << "@@                            @@\n";
+			cout << "@@                            @@\n";
+			cout << "@@                            @@\n";
+			cout << "@@                            @@\n";
+			cout << "@@                            @@\n";
+			cout << "@@                            @@\n";
+			cout << "@@                            @@\n";
+			cout << "@@                            @@\n";
+			cout << "@@                            @@\n";
+			cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n";
+			xPos++;
+			for (unsigned long long int x = 0; x < 100000; x++) {
+				;
+			}
 		}
 	}
 }
