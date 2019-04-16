37cff0f644493a5c824f6ffdaf0a239b236cfe43
Methsaan <methsaan@gmail.com>
2019-04-16 06:39:32 -0400

    add task to file

--- usefulprograms/tasktracker.cpp
+++ usefulprograms/tasktracker.cpp
@@ -3,27 +3,32 @@
 #include <cstdlib>
 #include <cstring>
 
-void numOfLines(FILE *file){
-	while (!feof(file)){
-	}
-}
 int main(int argc, char **argv){
-	FILE *fp = fopen("taskfile", "w");
-	FILE *fp2 = fopen("taskfile", "r");
+	printf("'c' to create new to do list, 'k' to keep original list: ");
+	char option = getchar();
+	if (option == 'c') {
+		FILE *clearfile = fopen("taskfile", "w");
+		fclose(clearfile);
+	}
 	char task[3000];
-	char taskToRead[3000];
+	int numOfTasks = 0;
+	getchar();
 	while (1) {
 		printf("Enter task: ");
 		fgets(task, 3000, stdin);
 		if (strcmp(task, "q\n") == 0) {
-			fclose(fp);
 			break;
-		}else if (strcmp(task, "p\n") == 0){
-			fscanf(fp2, "%s", taskToRead);
-			printf("task: %s", taskToRead);
+		}else if (strcmp(task, "t\n") == 0) {
+			system("cat taskfile");
 		}else {
-			printf("%s", task);
-			fprintf(fp, "%s", task);
+			if (option == 'c') {
+				FILE *fp = fopen("taskfile", "a");
+				numOfTasks++;
+				fprintf(fp, "%d %s", numOfTasks, task);
+				fclose(fp);
+			}else {
+				printf("Can't add task\n");
+			}
 		}
 	}
 }
