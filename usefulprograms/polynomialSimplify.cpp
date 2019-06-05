8da92c0348f798d92e163dece449eecbe0afbc75
Methsaan <methsaan@gmail.com>
2019-06-05 09:53:18 -0400

    simplify first degree polynomial

---
+++ usefulprograms/polynomialSimplify.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <iterator>
+
+using namespace std;
+
+class term {
+	public:
+		int coefficient;
+		string numPolarity;
+		string variable;
+};
+
+int isIn(string str, string arrstr[], int len) {
+	for (int x = 0; x < len; x++) {
+		if (arrstr[x] == str) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	int numOfTerms;
+	string variables[100];
+	cout << "Enter number of terms in the polynomial: ";
+	cin >> numOfTerms;
+	term polynomial[numOfTerms];
+	for (int x = 0; x < numOfTerms; x++) {
+		cin >> polynomial[x].coefficient >> polynomial[x].variable;
+		polynomial[x].numPolarity = polynomial[x].coefficient < 0 ? "" : "+";
+	}
+	int varcnt = 0;
+	for (int x = 0; x < sizeof(polynomial)/sizeof(*polynomial); x++) {
+		for (int y = 0; y < sizeof(variables)/sizeof(*variables); y++) {
+			if (!isIn(polynomial[x].variable, variables, 100)) {
+				variables[varcnt] = polynomial[x].variable;
+				varcnt++;
+			}
+		}
+	}
+	term simpPolynomial[varcnt];
+	int tot;
+	for (int x = 0; x < varcnt; x++) {
+		tot = 0;
+		for (int y = 0; y < numOfTerms; y++) {
+			if (polynomial[y].variable == variables[x]) {
+				tot += polynomial[y].coefficient;
+			}
+		}
+		simpPolynomial[x].coefficient = tot;
+		simpPolynomial[x].numPolarity = tot < 0 ? "" : "+";
+		simpPolynomial[x].variable = variables[x];
+	}
+	cout << "Polynomial: ";
+	for (int x = 0; x < numOfTerms; x++) {
+		cout << polynomial[x].numPolarity << polynomial[x].coefficient << polynomial[x].variable;
+	}
+	cout << endl;
+	cout << "Simplified polynomial: ";
+	for (int x = 0; x < varcnt; x++) {
+		cout << simpPolynomial[x].numPolarity << simpPolynomial[x].coefficient << simpPolynomial[x].variable;
+	}
+	cout << endl;
+}
--- usefulprograms/quadArea.cpp
+++ usefulprograms/quadArea.cpp
@@ -16,7 +16,14 @@ class quadrilateral {
 		double area;
 		string type;
 		quadrilateral(x1, y1, x2, y2, x3, y3, x4, y4) {
-			coord1[0] = min(min(x1, x2), min(x3, x4)) min(x1, x2)
+			coord1[0] = min(min(min(x1, x2), min(x3, x4)) max(min(x1, x2), min(x3, x4)));
+			coord1[1] = min(min(max(y1, y2), max(y3, y4)) max(min(y1, y2), min(y3, y4)));
+			coord2[0] = min(max(min(
+			coord2[1]
+			coord3[0]
+			coord3[1]
+			coord4[0]
+			coord4[1]
 		}
 		void findType() {
 			if () {
