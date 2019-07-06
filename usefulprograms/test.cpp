938913750de421899bce4b46f8d758481555dbce
Methsaan <methsaan@gmail.com>
2019-07-06 18:25:50 -0400

    include calculator object

--- usefulprograms/test.cpp
+++ usefulprograms/test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include "calculatorsci.cpp"
 
 using namespace std;
 
@@ -18,7 +19,7 @@ void replaceAll(std::string& subject, const std::string& search, const std::stri
 	}
 }
 int stringIndex(string str, string substring) {
-	size_t found = str.find(str1); 
+	size_t found = str.find(substring); 
 	if (found != string::npos)
 		return found;
 }
@@ -49,13 +50,6 @@ void encrypt(string *equation) {
 	replaceAll(portion + "^" + portion2, "pow(" + portion + "," + portion2 + ")");
 }
 int main(int argc, char *argv[]) {
-	cout << contains("abcdefghijklmnopqrstuv", "lmnop") << endl;
-	cout << contains("wrexcqwcxecz", "ypmnoyp") << endl;
-	string a = "hello dude woh dudes ya dude";
-	string s = "dude";
-	string r = "man";
-	replaceAll(a, s, r);
-	cout << a << endl;
 	string equation = "e+PI-3^4";
 	encrypt(&equation);
 	cout << equation << endl;
