374029bafd57e7910088d0050c77ff91f3da40cf
Methsaan <methsaan@gmail.com>
2019-07-08 10:43:47 -0400

    display answer

--- usefulprograms/calcscimain.cpp
+++ usefulprograms/calcscimain.cpp
@@ -3,13 +3,70 @@
 #include <cmath>
 #include <cstdlib>
 #include "calculatorsci.cpp"
+#include <cstring>
 
 using namespace std;
 
+string reverse(string str) {
+	string str2 = "";
+	for (int x = str.size(); x > -1; x--) {
+		str2 += str.substr(x, 1);
+	}
+	return str2;
+}
+int contains(string str, string substring) {
+	if (str.find(substring) != std::string::npos) {
+		return 1;
+	}
+	return 0;
+}
+void replaceAll(std::string& subject, const std::string& search, const std::string& replace) {
+	size_t pos = 0;
+	while((pos = subject.find(search, pos)) != std::string::npos) {
+		subject.replace(pos, search.length(), replace);
+		pos += replace.length();
+	}
+}
+int stringIndex(string str, string substring) {
+	size_t found = str.find(substring); 
+	if (found != string::npos) {
+		return found;
+	}
+}
+bool is_number(std::string& s) {
+	std::string::const_iterator it = s.begin();
+	while (it != s.end() && std::isdigit(*it)) ++it;
+	return !s.empty() && it == s.end();
+}
+string encrypt(string equation) {
+	string equation2 = equation;
+	replaceAll(equation2, "e", "c.e");
+	replaceAll(equation2, "PI", "c.PI");
+	replaceAll(equation2, "x", "*");
+	string portion;
+	string portion2;
+	string tempstr;
+	if (contains(equation2, "^")) {
+		for (int x = stringIndex(equation2, "^")-1;; x--) {
+			tempstr = equation2.substr(x, 1);
+			if ((!is_number(tempstr)) && (equation2.substr(x, 1) != ".")) {
+				break;
+			}
+			portion += equation2.substr(x, 1);
+		}
+		for (int x = stringIndex(equation2, "^")+1;; x++) {
+			tempstr = equation2.substr(x, 1);
+			if ((!is_number(tempstr)) && (equation2.substr(x, 1) != ".")) {
+				break;
+			}
+			portion2 += equation2.substr(x, 1);
+		}
+	}
+	replaceAll(equation2, reverse(portion) + "^" + portion2, "pow(" + reverse(portion) + "," + portion2 + ")");
+	return equation2;
+}
 int main(int argc, char *argv[]) {
 	calculator c;
-	fstream fo;
-	fo.open("calcfile.cpp");
 	cout << "_____________________________________________________" << endl;
 	cout << "| _________________________________________________ |" << endl;
 	cout << "| |                                               | |" << endl;
@@ -39,19 +96,24 @@ int main(int argc, char *argv[]) {
 	cout << "| ################################### |___| ######  |" << endl;
 	cout << "|___________________________________________________|" << endl;
 	while (true) {
+		fstream fo;
+		fo.open("calcfile.cpp");
 		string equation;
 		cin >> equation;
+		system("clear");
+		equation = encrypt(equation);
+		fo << "#include <iostream>" << endl;
 		fo << "#include <cmath>" << endl;
-		fo << "#include <iostream>" << endl << endl;
+		fo << "#include <cstring>" << endl;
+		fo << "#include \"calculatorsci.cpp\"" << endl << endl;
+		fo << "using namespace std;" << endl << endl;
 		fo << "calculator c;" << endl << endl;
 		fo << "int main(int argc, char *argv[]) {" << endl;
-		fo << "\tstring equation = " << equation << ";" << endl;
-		fo << "\tdouble answer;" << endl;
-		fo << "\tencrypt(equation, &answer);" << endl;
+		fo << "\tdouble answer = " << equation << ";" << endl;
 		fo << "\tcout << \"_____________________________________________________\" << endl;" << endl;
 		fo << "\tcout << \"| _________________________________________________ |\" << endl;" << endl;
 		fo << "\tcout << \"| |                                               | |\" << endl;" << endl;
-		fo << "\tcout << \"| |                                               | |\" << endl;" << endl;
+		fo << "\tcout << \"| |                                          \" << answer << \"     | |\" << endl;" << endl;
 		fo << "\tcout << \"| |_______________________________________________| |\" << endl;" << endl;
 		fo << "\tcout << \"|                                                   |\" << endl;" << endl;
 		fo << "\tcout << \"| ################################################  |\" << endl;" << endl;
@@ -77,6 +139,9 @@ int main(int argc, char *argv[]) {
 		fo << "\tcout << \"| ################################### |___| ######  |\" << endl;" << endl;
 		fo << "\tcout << \"|___________________________________________________|\" << endl;" << endl;
 		fo << "}" << endl;
+		fo.close();
+		system("g++ calcfile.cpp");
+		system("./a.out");
+		system("g++ calcscimain.cpp");
 	}
-	fo.close();
 }
--- usefulprograms/test.cpp
+++ usefulprograms/test.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include <cmath>
+#include <cstring>
 #include "calculatorsci.cpp"
 
 using namespace std;
 
 calculator c;
+string reverse(string str) {
+	string str2 = "";
+	for (int x = str.size(); x > -1; x--) {
+		str2 += str.substr(x, 1);
+	}
+	return str2;
+}
 int contains(string str, string substring) {
 	if (str.find(substring) != std::string::npos) {
 		return 1;
@@ -20,37 +28,39 @@ void replaceAll(std::string& subject, const std::string& search, const std::stri
 }
 int stringIndex(string str, string substring) {
 	size_t found = str.find(substring); 
-	if (found != string::npos)
+	if (found != string::npos) {
 		return found;
+	}
 }
 bool is_number(std::string& s) {
 	std::string::const_iterator it = s.begin();
 	while (it != s.end() && std::isdigit(*it)) ++it;
 	return !s.empty() && it == s.end();
 }
-void encrypt(string *equation) {
-	replaceAll(*equation, "e", "c.e");
-	replaceAll(*equation, "PI", "c.PI");
+string encrypt(string equation) {
+	string equation2 = equation;
+	replaceAll(equation2, "e", "c.e");
+	replaceAll(equation2, "PI", "c.PI");
+	replaceAll(equation2, "x", "*");
 	string portion;
 	string portion2;
-	if (contains(*equation, "^")) {
-		for (int x = stringIndex(*equation, "^");; x--) {
-			if (!is_number(equation->substr(x, 1)) && equation->substr(x, 1) != ".") {
+	string tempstr;
+	if (contains(equation2, "^")) {
+		for (int x = stringIndex(equation2, "^")-1;; x--) {
+			tempstr = equation2.substr(x, 1);
+			if ((!is_number(tempstr)) && (equation2.substr(x, 1) != ".")) {
 				break;
 			}
-			portion += *equation.substr(x, 1);	
+			portion += equation2.substr(x, 1);
 		}
-		for (int x = stringIndex(*equation, "^");; x++) {
-			if (!is_number(equation->substr(x, 1)) && equation->substr(x, 1) != ".") {
+		for (int x = stringIndex(equation2, "^")+1;; x++) {
+			tempstr = equation2.substr(x, 1);
+			if ((!is_number(tempstr)) && (equation2.substr(x, 1) != ".")) {
 				break;
 			}
-			portion2 += equation->substr(x, 1);
+			portion2 += equation2.substr(x, 1);
 		}
 	}
-	replaceAll(portion + "^" + portion2, "pow(" + portion + "," + portion2 + ")");
-}
-int main(int argc, char *argv[]) {
-	string equation = "e+PI-3^4";
-	encrypt(&equation);
-	cout << equation << endl;
+	replaceAll(equation2, reverse(portion) + "^" + portion2, "pow(" + reverse(portion) + "," + portion2 + ")");
+	return equation2;
 }
