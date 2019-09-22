9299d9945195fe86589d560cc742e9e93a85eec1
Methsaan <methsaan@gmail.com>
2019-09-22 16:50:46 -0400

    printing hits

--- usefulprograms/functionRun.cpp
+++ usefulprograms/functionRun.cpp
@@ -1,19 +1,11 @@
 #include <iostream>
 #include <cmath>
-
 #include <cstdlib>
 
 using namespace std;
 
 double f(double x) {
-	return  -x;
+	return  cos(x);
 }
 int main(int argc, char *argv[]) {
-	cout << f(9238) << endl;
-	cout << f(9387) << endl;
-	cout << f(29837) << endl;
-	cout << f(98327) << endl;
-	cout << f(92837) << endl;
-	cout << f(9823892) << endl;
-	cout << f(324) << endl;
 }
