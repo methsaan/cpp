a8acfe6794ea01b1864ab3335e397280069edc84
Methsaan <methsaan@gmail.com>
2019-12-06 15:45:47 -0500

    place values

--- usefulprograms/binary.cpp
+++ usefulprograms/binary.cpp
@@ -1,35 +1,3 @@
-//0	0
-//1	1	+1
-//2	10	+9
-//3	11	+1
-//4	100	+89
-//5	101	+1
-//6	110	+9
-//7	111	+1
-//8	1000	+889
-//9	1001	+1
-//10	1010	+9
-//11	1011	+1
-//12	1100	+89
-//13	1101	+1
-//14	1110	+9
-//15	1111	+1
-//16	10000	+8889
-//17	10001	+1
-//18	10010	+9
-//19	10011	+1
-//20	10100	+89
-//21	10101	+1
-//22	10110	+9
-//23	10111	+1
-//24	11000	+889
-//25	11001	+1
-//26	11010	+9
-//27	11011	+1
-//28	11100	+89
-//29	11101	+1
-//30	11110	+9
-//31	11111	+1
 #include <iostream>
 #include <cmath>
 #include <string>
@@ -37,11 +5,24 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-	int binaryNumbers[100];
-	for (int x = 0; x < 100; x++) {
-		string binarystr = "0000000";
-		for (int y = 0; y < 7; y++) {
-			binarystr.substr(y, 1);
+	int placevalues[] = {0, 0, 0, 0, 0, 0, 0, 0};
+	int decimal;
+	cout << "Enter decimal value: ";
+	cin >> decimal;
+	int y = 7;
+	int z = 7;
+	for (int x = 0; x < decimal; x++) {
+		int y = 7;
+		placevalues[y] += 1;
+		if (placevalues[y] == 2) {
+			placevalues[y] = 0;
+			placevalues[y-1] += 1;
+		} else {
+			break;
 		}
 	}
+	for (int x = 0; x < 8; x++) {
+		cout << placevalues[x];
+	}
+	cout << endl;
 }
