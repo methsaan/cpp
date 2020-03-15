7cd132ada9240e9f45d0d9b966d04b9b8d07f1e5
Methsaan <methsaan@gmail.com>
2020-03-15 19:39:59 -0400

    linear format

--- usefulprograms/algExpFinder.cpp
+++ usefulprograms/algExpFinder.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-	int x[8];
-	int y[] = {1, 2, 3, 4, 5, 6, 7, 8};
+	int x[] = {1, 2, 3, 4, 5, 6, 7, 8};
+	int y[8];
 	cout << "Enter pattern (8 terms):";
-	cin >> x[0] >> x[1] >> x[2] >> x[3] >> x[4] >> x[5] >> x[6] >> x[7];
+	cin >> y[0] >> y[1] >> y[2] >> y[3] >> y[4] >> y[5] >> y[6] >> y[7];
 	int inc[7];
 	for (int i = 0; i < 7; i++) {
-		inc[i] = x[i+1] - x[i];
+		inc[i] = y[i+1] - y[i];
 	}
 	int inc2[7];
 	for (int i = 0; i < 7; i++) {
@@ -20,11 +20,33 @@ int main(int argc, char *argv[]) {
 	string equation;
 	if (inc[0] == inc[1] && inc[1] == inc[2] && inc[2] == inc[3] && inc[3] == inc[4]) {
 		patternType = "Linear";
-		int intercept = y[0]*inc[0]-x[0];
-		equation = "y = " + to_string(inc[0]) + "x" + ((x == 0) ? "" : (((x > 0) ? " + " : " - ") + to_string(intercept)));
+		int intercept = x[0]*inc[0]-y[0];
+		if (inc[0] == 0 && intercept == 0) {
+			equation = "y = 0";
+		} else if (inc[0] == 1 && intercept == 0) {
+			equation = "y = x";
+		} else if (inc[0] == -1 && intercept == 0) {
+			equation = "y = -x";
+		} else if (inc[0] != 0 && inc[0] != 1 && inc[0] != -1 && intercept == 0) {
+			equation = "y = " + to_string(inc[0]) + "x";
+		} else if (inc[0] == 0 && intercept != 0) {
+			equation = "y = " + to_string(intercept);
+		} else if (inc[0] == 1 && intercept != 0) {
+			equation = "y = x + " + to_string(intercept);
+		} else if (inc[0] == -1 && intercept != 0) {
+			equation = "y = -x + " + to_string(intercept);
+		} else {
+			equation = "y = " + to_string(inc[0]) + "x + " + to_string(intercept);
+		}
 	}else if (inc2[0] == inc2[1] && inc2[1] == inc2[2] && inc2[2] == inc2[3] && inc2[3] == inc2[4]) {
 		patternType = "Exponential";
-		equation = "exp";
+		int expInc[] = {y[1]-y[0], y[2]-y[1], y[3]-y[2], y[4]-y[3], y[5]-y[4], y[6]-y[5], y[7]-y[6]};
+		for (int x = 0; x < 7; x++) {
+			cout << expInc[x] << endl;
+		}
+		int expIncInc[] = {expInc[1]-expInc[0], expInc[2]-expInc[1], expInc[3]-expInc[2], expInc[4]-expInc[3], expInc[5]-expInc[4], expInc[6]-expInc[5]};
+		int expDiff = y[0] - pow(x[0], expIncInc[0]);
+		equation = "y = x^" + to_string(expIncInc[0]) + (expDiff < 0 ? to_string(expDiff) : ("+" + to_string(expDiff)));
 	}
 	cout << "Pattern type: " << patternType << endl;
 	cout << "Equation: " << equation << endl;
