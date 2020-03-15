a931a6a7007970782a0200c09dfd05718d3a20b1
Methsaan <methsaan@gmail.com>
2020-03-15 19:39:59 -0400

    linear format

--- usefulprograms/functionRun.cpp
+++ usefulprograms/functionRun.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include <cmath>
+
 #include <cstdlib>
 
 using namespace std;
 
 double f(double x) {
-	return  cos(x);
+	return  2*pow(x, 2)-2;
 }
 int main(int argc, char *argv[]) {
+	cout << f(-5) << endl;
+	cout << f(-4) << endl;
+	cout << f(-3) << endl;
+	cout << f(-2) << endl;
+	cout << f(-1) << endl;
+	cout << f(0) << endl;
+	cout << f(1) << endl;
+	cout << f(2) << endl;
+	cout << f(3) << endl;
+	cout << f(4) << endl;
+	cout << f(5) << endl;
 }
