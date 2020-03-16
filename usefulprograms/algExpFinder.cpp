7582d15c86b2913c63ec216fe44ce54031dc736b
Methsaan <methsaan@gmail.com>
2020-03-16 18:17:04 -0400

    intercept simplification

--- usefulprograms/algExpFinder.cpp
+++ usefulprograms/algExpFinder.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(int argc, char *argv[]) {
 	int x[] = {1, 2, 3, 4, 5, 6, 7, 8};
 	int y[8];
-	cout << "Enter pattern (8 terms):";
+	cout << "Enter pattern (8 terms): ";
 	cin >> y[0] >> y[1] >> y[2] >> y[3] >> y[4] >> y[5] >> y[6] >> y[7];
 	int inc[7];
 	for (int i = 0; i < 7; i++) {
@@ -20,7 +20,7 @@ int main(int argc, char *argv[]) {
 	string equation;
 	if (inc[0] == inc[1] && inc[1] == inc[2] && inc[2] == inc[3] && inc[3] == inc[4]) {
 		patternType = "Linear";
-		int intercept = x[0]*inc[0]-y[0];
+		int intercept = y[0] - x[0]*inc[0];
 		if (inc[0] == 0 && intercept == 0) {
 			equation = "y = 0";
 		} else if (inc[0] == 1 && intercept == 0) {
@@ -32,11 +32,11 @@ int main(int argc, char *argv[]) {
 		} else if (inc[0] == 0 && intercept != 0) {
 			equation = "y = " + to_string(intercept);
 		} else if (inc[0] == 1 && intercept != 0) {
-			equation = "y = x + " + to_string(intercept);
+			equation = "y = x " + (intercept < 0 ? "- " : "+ ") + to_string(abs(intercept));
 		} else if (inc[0] == -1 && intercept != 0) {
-			equation = "y = -x + " + to_string(intercept);
+			equation = "y = -x " + (intercept < 0 ? "- " : "+ ") + to_string(abs(intercept));
 		} else {
-			equation = "y = " + to_string(inc[0]) + "x + " + to_string(intercept);
+			equation = "y = " + to_string(inc[0]) + "x " + (intercept < 0 ? "- " : "+ ") + to_string(abs(intercept));
 		}
 	}else if (inc2[0] == inc2[1] && inc2[1] == inc2[2] && inc2[2] == inc2[3] && inc2[3] == inc2[4]) {
 		patternType = "Exponential";
