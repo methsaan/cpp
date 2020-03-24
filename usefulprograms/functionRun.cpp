9293ce84bbccb051dc9657edb4aee213d3777d9d
Methsaan <methsaan@gmail.com>
2020-03-24 11:26:24 -0400

    exponent identification redo

--- usefulprograms/functionRun.cpp
+++ usefulprograms/functionRun.cpp
@@ -6,18 +6,8 @@
 using namespace std;
 
 double f(double x) {
-	return  2*pow(x, 2)-2;
+	return  -pow(x, 2);
 }
 int main(int argc, char *argv[]) {
-	cout << f(-5) << endl;
-	cout << f(-4) << endl;
-	cout << f(-3) << endl;
-	cout << f(-2) << endl;
 	cout << f(-1) << endl;
-	cout << f(0) << endl;
-	cout << f(1) << endl;
-	cout << f(2) << endl;
-	cout << f(3) << endl;
-	cout << f(4) << endl;
-	cout << f(5) << endl;
 }
