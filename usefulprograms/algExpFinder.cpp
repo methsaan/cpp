300928a0d305ad7dd88ba695fd2d75943253a0cf
Methsaan <methsaan@gmail.com>
2020-03-24 11:26:24 -0400

    exponent identification redo

--- usefulprograms/algExpFinder.cpp
+++ usefulprograms/algExpFinder.cpp
@@ -40,15 +40,8 @@ int main(int argc, char *argv[]) {
 		} else {
 			equation = "y = " + to_string(inc[0]) + "x " + (intercept < 0 ? "- " : "+ ") + to_string(abs(intercept));
 		}
-	}else if (inc2[0] == inc2[1] && inc2[1] == inc2[2] && inc2[2] == inc2[3] && inc2[3] == inc2[4]) {
+	}else if (inc2[0] <= inc2[1] && inc2[1] <= inc2[2] && inc2[2] <= inc2[3] && inc2[3] <= inc2[4]) {
 		patternType = "Exponential";
-		int expInc[] = {y[1]-y[0], y[2]-y[1], y[3]-y[2], y[4]-y[3], y[5]-y[4], y[6]-y[5], y[7]-y[6]};
-		for (int x = 0; x < 7; x++) {
-			cout << expInc[x] << endl;
-		}
-		int expIncInc[] = {expInc[1]-expInc[0], expInc[2]-expInc[1], expInc[3]-expInc[2], expInc[4]-expInc[3], expInc[5]-expInc[4], expInc[6]-expInc[5]};
-		int expDiff = y[0] - pow(x[0], expIncInc[0]);
-		equation = "y = x^" + to_string(expIncInc[0]) + (expDiff < 0 ? to_string(expDiff) : ("+" + to_string(expDiff)));
 	}
 	cout << "Pattern type: " << patternType << endl;
 	cout << "Equation: " << equation << endl;
