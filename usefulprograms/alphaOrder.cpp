b93675835248428d5aa49cdbb5fa09a47dc62292
Methsaan <methsaan@gmail.com>
2020-07-10 11:07:24 -0400

    alphabetical order

--- usefulprograms/alphaOrder.cpp
+++ usefulprograms/alphaOrder.cpp
@@ -7,14 +7,33 @@
 
 using namespace std;
 
+int abcGreater(string word1, string word2) {
+	string w1;
+	string w2;
+	if (word1.length() > word2.length()) {
+		w1 = word2;
+		w2 = word1;
+	} else {
+		w2 = word1;
+		w1 = word2;
+	}
+	for (int x = 0; x < w1.length(); x++) {
+		if (word1[x] < word2[x]) {
+			return 0;
+		} else if (word1[x] > word2[x]) {
+			return 1;
+		}
+	}
+}
+
 int main(int argc, char *argv[]){
 	string words[100];
 	int len = 0;
 	while (1) {
-		string ordinal = (len+1)%10 == 1 ? "st" : ((len+1)%10 == 2) ? "nd" : ((len+1)%10 == 3) ? "rd" : "th";
-		cout << "Enter " << len+1 << ordinal << " word (*<Q>* to quit): ";
+		string ordinal = ((len+1)%10 == 1 && len != 10) ? "st" : ((len+1)%10 == 2 && len != 11) ? "nd" : ((len+1)%10 == 3 && len != 12) ? "rd" : "th";
+		cout << "Enter " << len+1 << ordinal << " word (<q> to quit): ";
 		cin >> words[len];
-		if (words[len] == "*<Q>*") {
+		if (words[len] == "<q>") {
 			break;
 		}else {
 			len++;
@@ -23,7 +42,7 @@ int main(int argc, char *argv[]){
 	int range = len;
 	for (int i = len; i > 0; i--) {
 		for (int x = 0; x < range-1; x++) {
-			if (words[x][0] > words[x+1][0]) {
+			if (abcGreater(words[x], words[x+1])) {
 				string temp = words[x];
 				words[x] = words[x+1];
 				words[x+1] = temp;
