b51325290d2162fb068f11c7d4de3d5b1b11169b
Methsaan <methsaan@gmail.com>
2021-03-21 06:59:06 -0400

    reference

---
+++ tests/references.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int x = 10;
+	int& y = x;
+	cout << x << " " << y << endl;
+	x = 20;
+	cout << x << " " << y << endl;
+	cout << &x << " " << &y << endl;
+}
