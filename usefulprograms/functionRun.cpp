3a42d0af3045f97b9150379426ef32caf2dbdec2
Methsaan Wickramasinghe <methsaan@gmail.com>
2023-08-19 08:31:40 -0400

    updates!

--- usefulprograms/functionRun.cpp
+++ usefulprograms/functionRun.cpp
@@ -6,11 +6,14 @@
 using namespace std;
 
 double f(double x) {
-	return  6*x-10;
+	return  x - 7;
 }
 int main(int argc, char *argv[]) {
+	cout << 2 << " | " << f(2) << endl;
+	cout << 3 << " | " << f(3) << endl;
+	cout << 4 << " | " << f(4) << endl;
+	cout << 5 << " | " << f(5) << endl;
 	cout << 6 << " | " << f(6) << endl;
 	cout << 7 << " | " << f(7) << endl;
 	cout << 8 << " | " << f(8) << endl;
-	cout << 9 << " | " << f(9) << endl;
 }
